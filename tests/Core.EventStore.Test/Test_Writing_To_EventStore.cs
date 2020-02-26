0205c1dc4728081a8b4e37aa362d06001c621b3b
younes.bachaeimoghad <younes.bachaeimoghad@ESAGP4CL37067>
2020-02-26 11:35:11 +0100

    unit test added

---
+++ tests/Core.EventStore.Test/Test_Writing_To_EventStore.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Core.EventStore.Test
+{
+    public class Test_Writing_To_EventStore
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
